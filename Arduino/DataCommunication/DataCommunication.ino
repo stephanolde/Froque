28f37f0e77538b509cb150ed04c55e6fd72242cf
Yannick <yanhave@gmail.com>
2017-12-18 14:49:24 +0100

    Update on communication between the Arduino and the Raspberry PI

--- Arduino/DataCommunication/DataCommunication.ino
+++ Arduino/DataCommunication/DataCommunication.ino
@@ -5,17 +5,17 @@
 
 #include "CmdMessenger.h"
 
-int numSens = 20;
+const int numSens = 20;
 // Location x, Location y, Distance z.
 int sensLoc [numSens][3] = {
-	{0, 1, 0}, {0, 2, 0}, {1, 0, 0}, {1, 2, 0},
-	{2, 1, 0}, {2, 3, 0}, {3, 0, 0}, {3, 2, 0},
-	{4, 1, 0}, {4, 3, 0}, {5, 0, 0}, {5, 2, 0},
-	{6, 1, 0}, {6, 3, 0}, {7, 2, 0}, {8, 0, 0},
-	{8, 1, 0}, {9, 2, 0}, {9, 3, 0}, {10, 1, 0}
+	{0, 1, 0}, {0, 2, 1}, {1, 0, 2}, {1, 2, 3},
+	{2, 1, 4}, {2, 3, 5}, {3, 0, 6}, {3, 2, 7},
+	{4, 1, 8}, {4, 3, 9}, {5, 0, 10}, {5, 2, 11},
+	{6, 1, 12}, {6, 3, 13}, {7, 2, 14}, {8, 0, 15},
+	{8, 1, 16}, {9, 2, 17}, {9, 3, 18}, {10, 1, 19}
 };
 
-String sensLocString = String(sensLoc);
+//String sensLocString = String(sensLoc);
 
 /* Define available CmdMessenger commands */
 enum {
@@ -32,7 +32,15 @@ CmdMessenger c = CmdMessenger(Serial,',',';','/');
 
 /* callback */
 void on_setup_data(void) {
-    c.sendCmd(my_data_is, sensLocString);
+    for (int i = 0; i < numSens; i++) {
+        //c.sendCmd(my_x_is, sensLoc[i][0]);
+        //c.sendCmd(my_y_is, sensLoc[i][1]);
+        c.sendCmdStart(my_data_is);
+        c.sendCmdArg(sensLoc[i][0]);
+        c.sendCmdArg(sensLoc[i][1]);
+        c.sendCmdArg(sensLoc[i][2]);
+        c.sendCmdEnd();
+    }
 }
 
 /* callback */
--- Python/Comminucation.py
+++ Python/Comminucation.py
@@ -3,6 +3,7 @@
 # ------------------------------------------------------------------------------
 
 import PyCmdMessenger
+import time
 
 # Initialize an ArduinoBoard instance.  This is where you specify baud rate and
 # serial timeout.  If you are using a non ATmega328 board, you might also need
@@ -12,25 +13,22 @@ arduino = PyCmdMessenger.ArduinoBoard("/dev/ttyACM0", baud_rate=9600)
 # List of commands and their associated argument formats. These must be in the
 # same order as in the sketch.
 commands = [["setup_data", ""],
-            ["my_name_is", "s"],
-            ["sum_two_ints", "ii"],
-            ["sum_is", "i"],
+            ["my_data_is", "sss"],
             ["error", "s"]]
 
 # Initialize the messenger
 c = PyCmdMessenger.CmdMessenger(arduino, commands)
 
 # Send
-c.send("setup_data")
 # Receive. Should give ["my_data_is",[String(Data)],TIME_RECIEVED]
-msg = c.receive()
-print(msg)
 
-while true:
-	c.send("sensor_data")
+while True:
+	c.send("setup_data")
+	
+	for i in range(0, 20):
+            msg = c.receive()
+            print(msg)
+	
+	print("=================================================")
 	time.sleep(5)
 	
-	msg = c.receive()
-	print(msg)
-	
-	time.sleep(5)
